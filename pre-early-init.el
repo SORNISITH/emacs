8f445e56b25c25da4a9a0f33bec5c74c4ad26e79
void <nisith100394@gmail.com>
2026-05-09 18:04:45 +0700

    remove download folder

--- pre-early-init.el
+++ pre-early-init.el
@@ -1,6 +1,6 @@
 ;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
 ;;; NOTE: This must be placed in 'pre-early-init.el'.
-(setq user-emacs-directory (expand-file-name "var.lsp.br/" minimal-emacs-user-directory))
+(setq user-emacs-directory (expand-file-name "var2/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 (setq debug-on-error t)
 (setq minimal-emacs-gc-cons-threshold (* 64 1024 1024))
